5d31ff76b5567c57e2acb52f5ba8ada8b69c9ed7
pondrax <pondras.setiawan@gmail.com>
2021-07-15 01:35:25 +0700

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vite App</title>
+
+    <?= vite('dist/main.js') ?>
+</head>
+<body>
+	<div id="app"></div>
+</body>
+</html>
